c47cb16a6a98110c9a81b8af2673dfe3294374af
Cipppp <pirvuciprian37@gmail.com>
2021-01-16 15:24:27 +0200

    Fixed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -8,7 +8,7 @@ export const Balance = () => {
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
   return (
     <>
-      <h4>Your Balancee:</h4>
+      <h4>Your Balance:</h4>
       <h1>${total}</h1>
     </>
   )
